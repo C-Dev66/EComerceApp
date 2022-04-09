c50c103446c00f52fb5495d8d9c30b97fc2fe719
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-04-08 20:05:21 -0700

    create colors.dart

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const KShrinePink50 = Color(0xFFFEEAE6);
+const KShrinePink100 = Color(0xFFFEDBD0);
+const KShrinePink300 = Color(0xFFFBB8AC);
+const KShrinePink400 = Color(0xFFEAA4A4);
+
+const KShrineBrown900 = Color(0xFF442B2D);
+
+const KShrineErrorRed = Color(0xFFC5032B);
+
+const KShrineSurfaceWhite = Color(0xFFFFFBFA);
+const KShrineBackgroundWhite = Colors.white;
