d1ef55fe3e79718097a3c8feaf6a3a05ce3a8ecf
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-04-11 06:18:10 -0700

    Add Menu to Back Layer - Add branded icon

--- lib/home.dart
+++ lib/home.dart
@@ -135,7 +135,7 @@ class HomePage extends StatelessWidget {
 }
 
 */
-
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -153,5 +153,23 @@ class HomePage extends StatelessWidget {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
    return AsymmetricView(products: ProductsRepository.loadProducts(Category.all));
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
+import 'supplemental/asymmetric_view.dart';
+
+class HomePage extends StatelessWidget {
+  final Category category;
+
+  const HomePage({this.category = Category.all, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
   }
 }
