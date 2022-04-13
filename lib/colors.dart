5b562c036a5f477e0f2a13698d2ff5268a0dc1a8
carlosj <cj8002980@yahoo.com>
2022-04-13 03:58:28 -0700

    Red Color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 const KShrinePink50 = Color(0xFFFEEAE6);
-const KShrinePink100 = Color(0xFFFEDBD0);
+const KShrinePink100 = Color.fromARGB(255, 154, 23, 13);
 const KShrinePink300 = Color(0xFFFBB8AC);
 const KShrinePink400 = Color(0xFFEAA4A4);
 
-const KShrineBrown900 = Color(0xFF442B2D);
+const KShrineBrown900 = Color.fromARGB(255, 97, 96, 96);
 
 const KShrineErrorRed = Color(0xFFC5032B);
 
