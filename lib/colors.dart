1e60ab7291e2c0ac9bab5b76da37361018efb61f
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-05-15 18:24:40 -0700

    update colors, create screenshots

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 const KShrinePink50 = Color(0xFFFEEAE6);
-const KShrinePink100 = Color.fromARGB(255, 154, 23, 13);
+const KShrinePink100 = Color.fromARGB(255, 251, 163, 41);
 const KShrinePink300 = Color(0xFFFBB8AC);
 const KShrinePink400 = Color(0xFFEAA4A4);
 
-const KShrineBrown900 = Color.fromARGB(255, 97, 96, 96);
+const KShrineBrown900 = Color.fromARGB(255, 0, 0, 0);
 
 const KShrineErrorRed = Color(0xFFC5032B);
 
